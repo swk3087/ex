0094f91f11835708ebd45fbb3baeed732c2547f6
seo won kyeom <swk1072@gmail.com>
2025-11-20 22:40:30 +0900

    updated in moblie

--- install.sh
+++
@@ -1,3 +0,0 @@
-git config --global --add safe.directory /storage/emulated/0/dev/ex
-git config --global user.email "swk1072@gmail.com"
-git config --global user.name "seo won kyeom"
--- up.sh
+++
@@ -1,3 +0,0 @@
-git add .
-git commit -m "updated in moblie"
-git push -u origin main -f
